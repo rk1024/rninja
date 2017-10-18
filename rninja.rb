1169aac64b2aaddef7eb2d0bd596cc420da5ad81
rookie1024 <rookie1286@gmail.com>
2017-10-18 14:13:22 -0700

    Added basic support for Makefiles.

--- rninja.rb
+++ rninja.rb
@@ -7,23 +7,311 @@ require "set"
 require "subprocess"
 
 module RNinja
-  class FullBuilder
-    attr_accessor :defs, :defaults
+  class NinjaGenerator
+    attr_reader :rn_dir
 
     def initialize(d, rn_dir)
       @d = d.fork
+      @rn_dir = rn_dir
+    end
+
+    def file; "build.ninja" end
+
+    def sanitize_path(val) val.gsub(/(?<!\$)([:\s])/, "$\\1") end
+
+    def begin(l, defaults)
+      @l = l
+      @defaults = defaults
+    end
+
+    def pream_end; end
+
+    def end
+      @defaults.each do |default|
+        @l << "default"
+        emit_wrap(default)
+      end
+    end
+
+    private def emit_wrap(parts)
+      parts.each do |part|
+        if @l.peek.length > 0
+          if (@l.peek.length + part.length + 1) > 78
+            @l.peek << " $"
+            @l << "  #{part}"
+          else
+            @l.peek << " " << part
+          end
+        else
+          @l.trim << part
+        end
+      end
+    end
+
+    private def emit_opts(kvs)
+      kvs.each do |key, val|
+        @l << "#{key} ="
+        if val.is_a?(String)
+          @l.peek << " " << val
+        else
+          emit_wrap(val)
+        end
+      end
+    end
+
+    def emit_set(name, val) emit_opts(name => val) end
+
+    def emit_rule(name, opts)
+      @l << "rule #{name}"
+
+      @l.fmt with_indent: "  " do
+        emit_opts(opts)
+      end
+
+      @l.sep unless opts.empty?
+    end
+
+    def emit_build(name, also, with, from, imply, after, opts)
+      parts = ["build", *name]
+      parts << "|" unless also.empty?
+      parts.concat(also)
+      parts[-1] = "#{parts[-1]}:"
+      parts << with
+      parts.concat(from)
+      parts << "|" unless imply.empty?
+      parts.concat(imply)
+      parts << "||" unless after.empty?
+      parts.concat(after)
+
+      parts.map!{|p| p.to_s }
+
+      @l << ""
+      emit_wrap(parts)
+
+      @l.fmt with_indent: "  " do
+        emit_opts(opts)
+      end
+
+      @l.sep unless opts.empty?
+    end
+
+    def cmd(script, args) [*%w[ninja -f], script, *args] end
+  end
+
+  class MakeGenerator
+    attr_reader :rn_dir
+
+    def initialize(d, rn_dir)
+      @d = d.fork
+      @has_mkdir = false
+      @rn_dir = rn_dir
+      @rules = {}
+      @dirs = Set[]
+    end
+
+    def file; "Makefile" end
+
+    def sanitize_path(val) val.gsub(/(?<!\\)([#])/, "\\\\1") end
+
+    def begin(l, defaults)
+      @l = l
+      @defaults = defaults
+    end
+
+    def pream_end
+      @l << ".SECONDARY:" << ".DELETE_ON_ERROR:"
+      @l.sep
+
+      @l << ".PHONY: $(rn_d)/.defaults.rnj" << "$(rn_d)/.defaults.rnj: #{@defaults.join(" ")}"
+      @l.sep
+    end
+
+    def end
+      q = @dirs.to_a
+
+      until q.empty?
+        dir = File.dirname(q.shift)
+
+        q << dir if @dirs.add?(dir)
+      end
+
+      @dirs.reject!{|d| File.dirname(d) == d }
+
+      unless @has_mkdir
+        @l.sep
+        emit_set(:rn_mkdir, "mkdir")
+        @l.sep
+
+        emit_rule(:rn_mkdir, {"command" => "$rn_mkdir $out"})
+      end
+
+      @dirs.each do |itm|
+        parent = File.dirname(itm)
+        parent = nil if File.dirname(parent) == parent
+        emit_build([itm], [], :rn_mkdir, [*parent], [], [], {
+          "description" => "mkdir #{itm}"
+        }, is_mkdir: true)
+      end
+    end
+
+    def get(var, vars, expanded: true, skip: Set[])
+      var = var.to_sym
+      val = vars.fetch(var) do
+        return yield if block_given?
+        raise @d.error_r("unknown variable #{@d.hl(var)}")
+      end
+      expanded ? expand(val, vars, skip: skip) : val
+    end
+
+    def expand(str, vars, extra: nil, skip: Set[])
+      return str.map{|i| expand(i, vars, extra: extra, skip: skip) } unless str.is_a?(String)
+
+      str = str.gsub(/\$\n/, "")
+      str.gsub!(/\$([\s:\$])/, "\\1")
+
+      str.gsub!(/\$(?:(\w+)\b|\{(\w+)\})/) do |match|
+        var = ($1 || $2).to_sym
+
+        raise @d.error_r("Can't resolve recursive variable reference!") if skip.include?(var)
+
+        catch(:found) do
+          throw(:found, extra[var]) if extra && extra.include?(var)
+
+          get(var, vars, skip: skip | [var]) { match }
+        end
+      end
+
+      str
+    end
+
+    private def fix_vars(str)
+      return str.map{|i| fix_vars(i) } unless str.is_a?(String)
+
+      str.gsub(/\$(?:(\w+)\b|\{(\w+)\})/) {|m| "$(#{$1 || $2})" }
+    end
+
+    private def resplit(str)
+      return str.flat_map{|i| resplit(i) } unless str.is_a?(String)
+
+      str.split(/\s+|('.*'|".*")/).reject{|s| !s || s.empty? }
+    end
+
+    private def emit_wrap(parts, recipe:)
+      parts.each do |part|
+        if @l.peek.length > 0
+          if (@l.peek.length + part.length + 1) > (recipe ? 78 : 79)
+            @l.peek << (recipe ? " \\" : "\\")
+            @l << " #{part}"
+          else
+            @l.peek << " " << part
+          end
+        else
+          @l.trim << (recipe ? "\t#{part}" : part)
+        end
+      end
+    end
+
+    private def emit_opts(kvs, recipe:)
+      kvs.each do |key, val|
+        @l << "#{key} ="
+        if val.is_a?(String)
+          @l.peek << " " << val
+        else
+          emit_wrap(val, recipe: recipe)
+        end
+      end
+    end
+
+    def emit_set(name, val)
+      emit_opts({name => fix_vars(val)}, recipe: false)
+    end
+
+    def emit_rule(name, opts)
+      @has_mkdir = true if name == "rn_mkdir"
+      @rules[name] = opts
+    end
+
+    def emit_build(name, also, with, from, imply, after, opts, is_mkdir: false)
+      is_phony = with == "phony"
+
+      vars = opts.map{|k, v| [k.to_sym, v.to_s] }.to_h
+
+      parts = [*name, *also]
+
+      unless is_mkdir || is_phony
+        parts.map{|p| File.dirname(p) }.select{|d| File.dirname(d) != d }.each do |dir|
+          @dirs << dir
+          imply << dir
+        end
+      end
+
+      if is_phony
+        @l << ".PHONY:"
+        emit_wrap(parts, recipe: false)
+      end
+
+      parts[-1] = "#{parts[-1]}:"
+
+      parts.concat(from)
+      parts.concat(imply)
+      parts << "|" unless after.empty?
+      parts.concat(after)
+
+      parts = resplit(fix_vars(expand(parts.map!{|p| p.to_s }, vars)))
+
+      parts.each{|n| raise @d.fatal_r("invalid rule part '#{n}'") if n =~ /\s/ }
+
+      @l << ""
+      emit_wrap(parts, recipe: false)
+
+      unless is_phony
+        rule = @rules[with]
+
+        raise @d.fatal_r("no rule found for #{@d.hl(with)}") unless rule
+
+        parts = [rule["command"]]
+
+        raise @d.fatal_r("bad command for rule #{@d.hl(with)}") unless parts.any?
+
+        parts = resplit(fix_vars(expand(parts.map!{|p| p.to_s }, vars, extra: {
+          in: imply.empty? ? "$^" : resplit(fix_vars(expand(from, vars))).join(" "),
+          out: also.empty? && name.length == 1 ? "$@" : resplit(fix_vars(expand(name, vars))).join(" "),
+        })))
+
+        parts[0] = "@#{parts[0]}" unless parts.empty?
+
+        parts.each{|n| @d.warn("maybe invalid recipe part '#{n}'") if n =~ /\s/ }
+
+        @l << ""
+        emit_wrap(parts, recipe: true)
+
+        catch(:stop) { @l << "\t@echo \e[1m[RNinja]\e[0m #{fix_vars(vars.fetch(:description) { throw(:stop) })}" }
+
+        @l.sep unless parts.empty?
+      end
+    end
+
+    def cmd(script, args) [*%w[make -f], script, *args] end
+  end
+
+  class FullBuilder
+    attr_accessor :defs, :defaults
+
+    def initialize(d, generator)
+      @d = d.fork
+      @generator = generator
       @rules = {}
       @infos = {}
       @defs = []
       @vars = {}
       @defaults = []
 
-      set builddir: rn_dir
+      set rn_d: @generator.rn_dir
     end
 
-    def sanitize(val, colon: false, space: false, vars: false)
-      return val.map{|v| sanitize(v, colon: colon, space: space, vars: vars) } if val.is_a?(Enumerable)
-      val.to_s.gsub(/(?<!\$)([#{":" if colon}#{"\\s" if space}#{"\\$" if vars}])/, "$\\1")
+    def sanitize_path(val)
+      return val.map{|v| sanitize_path(v) } if val.is_a?(Enumerable)
+      @generator.sanitize_path(val.to_s)
     end
 
     def massage(val)
@@ -44,7 +332,7 @@ module RNinja
       str.gsub!(/\$(?:(\w+)\b|\{(\w+)\})/) do |match|
         var = ($1 || $2).to_sym
 
-        @d.error_r("Can't resolve recursive variable reference!") if skip.include?(var)
+        raise @d.error_r("Can't resolve recursive variable reference!") if skip.include?(var)
 
         catch(:found) do
           throw(:found, extra[var]) if extra && extra.include?(var)
@@ -58,7 +346,7 @@ module RNinja
 
     def get(var, expanded: true, skip: Set[])
       var = var.to_sym
-      val = @vars.fetch(var) { @d.error_r("Unknown variable '#{var}'") }
+      val = @vars.fetch(var) { raise @d.error_r("Unknown variable '#{var}'") }
       expanded ? expand(val, skip: skip) : val
     end
 
@@ -83,7 +371,7 @@ module RNinja
       if build || from
         @rules[[build, from].map! do |set|
           case set
-            when nil; @d.error_r("Both build: and from: must be specified, or neither")
+            when nil; raise @d.error_r("Both build: and from: must be specified, or neither")
             when Set; set
             else Set[*set]
           end
@@ -102,7 +390,7 @@ module RNinja
         with = @rules.fetch([name, from].map! do |set|
           set = [*set]
           Set[*set.map{|n| File.extname(n) }]
-        end) { @d.error_r("No implicit rule found to build #{name.join(" ")} from #{from.empty? ? "nothing" : from.join(" ")}.") }
+        end) { raise @d.error_r("No implicit rule found to build #{name.join(" ")} from #{from.empty? ? "nothing" : from.join(" ")}.") }
       end
 
       if !opts.include?(:description) && @infos.include?(with)
@@ -113,7 +401,7 @@ module RNinja
       end
 
 
-      [name, also, from, imply, after].each{|v| v.map!{|i| sanitize(i, colon: true, space: true) } }
+      [name, also, from, imply, after].each{|v| v.map!{|i| sanitize_path(i) } }
 
       @defs << [:build, name, also, with, from, imply, after, massage_kv(opts)]
       @defaults << name if default
@@ -155,116 +443,70 @@ module RNinja
     end
   end
 
-  def self.run(rn_dir:, &block)
+  def self.run(rn_dir:, gen: :ninja, &block)
     @@d.pos("RNinja.run") do
       b = nil
 
       run_diag do
         FileUtils.mkdir_p(rn_dir)
 
-        b = FullBuilder.new(@@d, rn_dir)
+        gen = case gen
+          when :ninja; NinjaGenerator
+          when :make; MakeGenerator
+          else nil
+        end.new(@@d, rn_dir)
+
+        b = FullBuilder.new(@@d, gen)
         b.instance_eval(&block)
 
-        ninja_file = File.join(rn_dir, "build.ninja")
+        build_script = File.join(rn_dir, gen.file)
 
-        File.open(ninja_file, "w") do |file|
+        File.open(build_script, "w") do |file|
           was_var = false
+          is_pream = true
           file << LineWriter.lines do |l|
-            emit_wrap = lambda do |parts|
-              parts.each do |part|
-                if l.peek.length > 0
-                  if (l.peek.length + part.length + 1) > 78
-                    l.peek << " $"
-                    l << "  #{part}"
-                  else
-                    l.peek << " " << part
-                  end
-                else
-                  l.trim << part
-                end
-              end
-            end
-
-            emit_opts = lambda do |kvs|
-              kvs.each do |key, val|
-                l << "#{key} ="
-                if val.is_a?(String)
-                  l.peek << " " << val
-                else
-                  emit_wrap.(val)
-                end
-              end
-            end
+            gen.begin(l, b.defaults)
 
             b.defs.each do |type, *args|
-              is_var = false
+              is_var = type == :set
+
+              if was_var
+                if is_var
+                  l.trim
+                else
+                  if is_pream
+                    is_pream = false
+
+                    gen.pream_end
+                  end
+                end
+              end
 
               case type
-                when :set
-                  is_var = true
-
-                  l.trim if was_var
-                  (name, val) = args
-                  emit_opts.(name => val)
-                  l.sep
-
-                when :rule
-                  (name, opts) = args
-
-                  l << "rule #{name}"
-
-                  l.fmt with_indent: "  " do
-                    emit_opts.(opts)
-                  end
-
-                  l.sep unless opts.empty?
-                when :build
-                  (name, also, with, from, imply, after, opts) = args
-
-                  parts = ["build", *name]
-                  parts << "|" unless also.empty?
-                  parts.concat(also)
-                  parts[-1] = "#{parts[-1]}:"
-                  parts << with
-                  parts.concat(from)
-                  parts << "|" unless imply.empty?
-                  parts.concat(imply)
-                  parts << "||" unless after.empty?
-                  parts.concat(after)
-
-                  parts.map!{|p| p.to_s }
-
-                  l << ""
-                  emit_wrap.(parts)
-
-                  l.fmt with_indent: "  " do
-                    emit_opts.(opts)
-                  end
-
-                  l.sep unless opts.empty?
-
-                else @d.fatal_r("Unknown deftype #{type.inspect}")
+                when :set; gen.emit_set(*args)
+                when :rule; gen.emit_rule(*args)
+                when :build; gen.emit_build(*args)
+                else raise @d.fatal_r("Unknown deftype #{type.inspect}")
               end
+
+              l.sep if is_var
 
               was_var = is_var
             end
 
             l.sep
 
-            b.defaults.each do |default|
-              l << "default"
-              emit_wrap.(default)
-            end
+            gen.end
 
             l.trim
           end << "\n"
         end
 
-        ninja_cmd = ["ninja", "-f", ninja_file, *ARGV]
+        build_cmd = gen.cmd(build_script, ARGV)
 
-        @@d.info(ninja_cmd.join(" "))
+        @@d.info(build_cmd.map{|a| a =~ /\s/ ? "\"#{a}\"" : a }.join(" "))
 
-        status = Subprocess.call(ninja_cmd)
+        status = Subprocess.call(["/usr/bin/env", *build_cmd])
 
         @@d.info("Ninja exited with code #{status.exitstatus}")
 
